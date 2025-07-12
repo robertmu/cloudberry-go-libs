92558fee4befe32ec26c91b5d13f2f283e84fd35
Robert Mu <dbx_c@hotmail.com>
2025-07-12 21:13:36 +0800

    feat(dbconn): Add support for Apache Cloudberry Database
    This commit refactors the database version parsing logic to support
    multiple database types, starting with Greenplum and Apache Cloudberry.
    The previous implementation was tightly coupled to Greenplum Database
    and used a simple string search to extract the version, which was not
    robust or extensible.
    
    Key changes:
    1. Introduces a DBType enum to identify the database (GPDB, CBDB).
    2. Replaces string manipulation with regular expressions in
    ParseVersionInfo for more accurate and reliable version parsing.
    3 . Adds helper functions (IsGPDB(), IsCBDB()) to easily check the
    database type.
    4. Updates unit tests in version_test.go to validate the new parsing
    logic for both supported databases.
    
    Additionally, this change lays the groundwork for resolving a similar
    version detection issue in the apache/cloudberry-gpbackup project.
    The backup utility currently cannot correctly identify Apache Cloudberry,
    and this foundational fix in the shared library is the first step
    toward enabling proper support in that tool.
    
    This new approach makes the system more maintainable and easier to
    extend with support for other database systems in the future.

--- dbconn/version.go
+++ dbconn/version.go
@@ -5,11 +5,40 @@ import (
 	"strings"
 
 	"github.com/blang/semver"
+	"github.com/cloudberrydb/gp-common-go-libs/gplog"
 )
 
+// DBType represents the type of database
+type DBType int
+
+const (
+	Unknown DBType = iota
+	GPDB           // Greenplum Database
+	CBDB           // Apache Cloudberry Database
+)
+
+const (
+	gpdbPattern = `\(Greenplum Database ([0-9]+\.[0-9]+\.[0-9]+)[^)]*\)`
+	cbdbPattern = `\(Apache Cloudberry ([0-9]+\.[0-9]+\.[0-9]+)[^)]*\)`
+)
+
+// String provides string representation of DBType
+func (t DBType) String() string {
+	switch t {
+	case GPDB:
+		return "Greenplum Database"
+	case CBDB:
+		return "Apache Cloudberry"
+	default:
+		return "Unknown Database"
+	}
+}
+
+// GPDBVersion represents version information for a database
 type GPDBVersion struct {
 	VersionString string
 	SemVer        semver.Version
+	Type          DBType
 }
 
 /*
@@ -26,26 +55,57 @@ func NewVersion(versionStr string) GPDBVersion {
 	version := GPDBVersion{
 		VersionString: versionStr,
 		SemVer:        semver.MustParse(versionStr),
+		Type:          GPDB, // Default to GPDB for tests
 	}
 	return version
 }
 
+// InitializeVersion parses database version string and returns version information
+// It can distinguish between Greenplum Database and Apache Cloudberry Database.
 func InitializeVersion(dbconn *DBConn) (dbversion GPDBVersion, err error) {
 	err = dbconn.Get(&dbversion, "SELECT pg_catalog.version() AS versionstring")
 	if err != nil {
 		return
 	}
-	versionStart := strings.Index(dbversion.VersionString, "(Greenplum Database ") + len("(Greenplum Database ")
-	versionEnd := strings.Index(dbversion.VersionString, ")")
-	dbversion.VersionString = dbversion.VersionString[versionStart:versionEnd]
 
-	pattern := regexp.MustCompile(`\d+\.\d+\.\d+`)
-	threeDigitVersion := pattern.FindStringSubmatch(dbversion.VersionString)[0]
-	dbversion.SemVer, err = semver.Make(threeDigitVersion)
+	// Determine database type and parse version
+	dbversion.ParseVersionInfo(dbversion.VersionString)
+
+	gplog.Info("Initialized database version - Full Version: %s, Database Type: %s, Semantic Version: %s",
+		dbversion.VersionString, dbversion.Type, dbversion.SemVer)
 	return
 }
 
-func StringToSemVerRange(versionStr string) semver.Range {
+func (dbversion *GPDBVersion) ParseVersionInfo(versionString string) {
+	dbversion.VersionString = versionString
+	dbversion.Type = Unknown
+
+	// Try to match each database type.
+	// We check for Apache Cloudberry first as its string may be a superset of others in the future.
+	if ver, ok := dbversion.extractVersion(cbdbPattern); ok {
+		dbversion.Type = CBDB
+		dbversion.SemVer = ver
+	} else if ver, ok := dbversion.extractVersion(gpdbPattern); ok {
+		dbversion.Type = GPDB
+		dbversion.SemVer = ver
+	}
+}
+
+func (dbversion GPDBVersion) extractVersion(pattern string) (semver.Version, bool) {
+	re := regexp.MustCompile(pattern)
+	matches := re.FindStringSubmatch(dbversion.VersionString)
+	if len(matches) < 2 {
+		return semver.Version{}, false
+	}
+
+	ver, err := semver.Make(matches[1])
+	if err != nil {
+		return semver.Version{}, false
+	}
+	return ver, true
+}
+
+func (dbversion GPDBVersion) StringToSemVerRange(versionStr string) semver.Range {
 	numDigits := len(strings.Split(versionStr, "."))
 	if numDigits < 3 {
 		versionStr += ".x"
@@ -55,16 +115,32 @@ func StringToSemVerRange(versionStr string) semver.Range {
 }
 
 func (dbversion GPDBVersion) Before(targetVersion string) bool {
-	validRange := StringToSemVerRange("<" + targetVersion)
+	validRange := dbversion.StringToSemVerRange("<" + targetVersion)
 	return validRange(dbversion.SemVer)
 }
 
 func (dbversion GPDBVersion) AtLeast(targetVersion string) bool {
-	validRange := StringToSemVerRange(">=" + targetVersion)
+	validRange := dbversion.StringToSemVerRange(">=" + targetVersion)
 	return validRange(dbversion.SemVer)
 }
 
 func (dbversion GPDBVersion) Is(targetVersion string) bool {
-	validRange := StringToSemVerRange("==" + targetVersion)
+	validRange := dbversion.StringToSemVerRange("==" + targetVersion)
 	return validRange(dbversion.SemVer)
+}
+
+func (dbversion GPDBVersion) IsGPDB() bool {
+	return dbversion.Type == GPDB
+}
+
+func (dbversion GPDBVersion) IsCBDB() bool {
+	return dbversion.Type == CBDB
+}
+
+func (srcVersion GPDBVersion) Equals(destVersion GPDBVersion) bool {
+	if srcVersion.Type != destVersion.Type {
+		return false
+	}
+
+	return srcVersion.SemVer.Major == destVersion.SemVer.Major
 }
